fd70ab2be38058da444855ec04d158b0e89c1cef
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2016-08-07 04:35:12 +0530

    understanding how reducers, action, connect and bindactioncreators work

---
+++ src/containers/book-list.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+	renderList() {
+		return this.props.books.map((book) => {
+			return (<li
+					onClick={() => this.props.selectBook(book)}
+					key={book.title}
+					className="list-group-item">
+					{book.title}
+					</li>);
+		});
+	}
+	render() {
+		return (
+			<ul className="list-group col-md-5">
+				{ this.renderList() }
+			</ul>
+		);
+	}
+}
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+function mapStateToProps(state) {
+	return {
+		books: state.books
+	}
+}
+export default connect(mapStateToProps,mapDispatchToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: "Javascript the good parts" },
+		{ title: "Harry Potter" },
+		{ title: "Rich Dad and Poor Dad" },
+		{ title: "Good Books" }
+	];
+}
